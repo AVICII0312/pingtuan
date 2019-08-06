bdbff8a8c9035994c7e170140a53c6ada8cb34a4
Avicii <Avicii0312@163.com>
2019-08-06 17:54:44 +0800

    完成 优惠券页面和规则页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,21 +4,5 @@ const app = getApp()
 
 Page({
 
-  toStamps:function(e){
-    console.log(e)
-    wx.navigateTo({
-      url:'../stamps/stamps'
-    })
-  },
-  toHelp:function(){
-    wx.navigateTo({
-      url:'../help/help'
-    })
-  },
-  toStamps:function(){
-    wx.navigateTo({
-      url:'../stamps/stamps'
-    })
-  }
 
 })
